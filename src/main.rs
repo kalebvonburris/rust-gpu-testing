d1f98bbc70701a7d296e105ec78144d711d766ab
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-11-24 00:42:37 -0900

    Working fib example!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,57 @@
-use rust_gpu_testing::run;
+#[macro_use]
+extern crate timeit;
 
-fn main() {
-    pollster::block_on(run());
+use rust_gpu_testing::*;
+
+use rayon::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    let numbers: Vec<_> = (0..65_000).collect();
+
+    println!("Benching GPU...");
+
+    timeit!({
+        execute_gpu(numbers.clone()).await.unwrap();
+    });
+
+    println!("Benching CPU...");
+
+    timeit!({
+        execute_cpu(numbers.clone());
+    });
+
+    let steps_gpu = execute_gpu(numbers.clone()).await.unwrap();
+    let steps_cpu = execute_cpu(numbers.clone());
+
+    for (index, gpu_ans) in steps_gpu.iter().enumerate() {
+        if *gpu_ans != steps_cpu[index] {
+            println!("GPU Answer: {} != CPU Answer: {}", gpu_ans, steps_cpu[index]);
+            panic!("Your code is wrong!");
+        }
+    }
+
+    println!("Answers recieved were identical.");
+
+}
+
+const MAX: u32 = 100000;
+
+fn execute_cpu(numbers: Vec<u32>) -> Vec<u32> {
+    return numbers.par_iter().map( |num| {
+        fib(*num)
+    }).collect()
+}
+
+fn fib(n: u32) -> u32 {
+    let mut second_num: u32 = 0;
+    let mut first_num: u32 = 1;
+    let mut current_num: u32 = 1;
+
+    for _ in 0..(n + 1) {
+        current_num = (second_num + first_num) % MAX;
+        second_num = first_num;
+        first_num = current_num;
+    }
+    return current_num;
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,7 +1,30 @@
-struct nothing {
-    i: u32,
+@group(0)
+@binding(0)
+var<storage, read_write> v_indices: array<u32>; // this is used as both input and output for convenience
+
+const MAX : u32 = 100000u;
+
+// The Collatz Conjecture states that for any integer n:
+// If n is even, n = n/2
+// If n is odd, n = 3n+1
+// And repeat this process for each new n, you will always eventually reach 1.
+// Though the conjecture has not been proven, no counterexample has ever been found.
+// This function returns how many times this recurrence needs to be applied to reach 1.
+fn fib(n: u32) -> u32{
+    var second_num: u32 = 0u;
+    var first_num: u32 = 1u;
+    var current_num: u32 = 1u;
+
+    for (var i = 0u; i <= n; i++) {
+        current_num = (second_num + first_num) % MAX;
+        second_num = first_num;
+        first_num = current_num;
+    }
+    return current_num;
 }
 
-fn frag() -> i32 {
-    return 0;
+@compute
+@workgroup_size(1)
+fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+    v_indices[global_id.x] = fib(v_indices[global_id.x]);
 }
